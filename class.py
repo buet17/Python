eab4429bc6b32048c53e042e58a0dd9e9cf16c5d
nour.buet@gmail.com <nour.buet@gmail.com>
2024-01-09 19:11:40 +0600

    Third comment

--- class.py
+++ class.py
@@ -13,17 +13,34 @@
 # print(p1.name, end=", ")
 # print(p1.age)
 
-class Person:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def __str__(self,):
-        return  f"{self.name}({self.age})"
+# class Person:
+#     def __init__(self,name,age):
+#         self.name=name
+#         self.age=age
+#     def __str__(self,):
+#         return  f"{self.name}({self.age})"
     
-    def x(self):
-        print("Hello my name is: " + self.name,self.age)
+#     def x(self):
+#         print("Hello my name is: " + self.name,self.age)
     
-p1=Person("John",35)
-print(p1)
+# p1=Person("John",35)
+# print(p1)
 
-p1.x()
+# p1.x()
+
+class baba:
+    car='BMW'
+    price='100 M'
+    home='10th floor'
+
+class child(baba):
+    bike='R15'
+    mobile='iphone 5'
+
+p1=baba()
+p2=child()
+print(child.bike)
+print(child.car)
+
+del p1
+del p2
